a369266c5ad7a23de8e64d01af4d2fac5d800272
SvetlanaKhobta <svetoch_1980@mail.ru>
2023-08-31 11:43:47 +0900

    recipes

---
+++ src/myRecipesComponent.js
@@ -0,0 +1,29 @@
+
+function MyRecipesComponent({label, image, calories, ingredients}){
+  
+    return(
+        <div >
+            
+            <div className="container ">
+                <h2>{label}</h2>
+            </div>
+            <div className="container ">
+                <img src={image}/>
+            </div>
+            <ul className="container list">
+                {ingredients.map((ingredient, index) => (
+                    <li key = {index}>
+                    <img className="chek" src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-check-multimedia-kiranshastry-gradient-kiranshastry.png " alt="icon"/>
+                    {ingredient}
+                    </li>
+                ))}
+            </ul>
+            <div className="container ">
+                <p>{calories.toFixed()} calories.</p>
+            </div>
+          
+
+        </div>
+    )
+}
+export default MyRecipesComponent;
